8fec7fa4f1e94b3310c5806f3d8303c5be94f1ec
mohsen-pashootan <64013260+mohsen-pashootan@users.noreply.github.com>
2020-07-11 20:26:23 +0430

    add a new javascript library named lodash . and add a pagination bootstrap template and work with its properties

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./common/pagination";
 import Like from "./common/like";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -18,6 +20,10 @@ class Movies extends Component {
     movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    console.log(page);
   };
 
   render() {
@@ -62,6 +68,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
