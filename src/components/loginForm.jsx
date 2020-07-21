b6b4dfecf1df2274843a244de099c81bcf0c964b
mohsen-pashootan <64013260+mohsen-pashootan@users.noreply.github.com>
2020-07-21 16:30:08 +0430

    handle first input change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
   render() {
     return (
       <div>
         <h1>Login Form</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={this.state.account.username}
+              onChange={this.handleChange}
+              autoFocus
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
