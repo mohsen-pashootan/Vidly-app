e9726798de5ac3fdba611c0f2bb64c51055ee4b2
mohsen-pashootan <64013260+mohsen-pashootan@users.noreply.github.com>
2020-07-21 18:29:56 +0430

    using a library for validating name "Joi"

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,7 +8,17 @@ class LoginForm extends Component {
     errors: {},
   };
 
-  validation = () => {
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
+
+  validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
@@ -33,7 +44,7 @@ class LoginForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
 
-    const errors = this.validation();
+    const errors = this.validate();
     this.setState({ errors: errors || {} });
     if (errors) return;
   };
