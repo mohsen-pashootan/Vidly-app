8b0e1d4ef1d4535ecf4460546b134f4401c5a7fe
mohsen-pashootan <64013260+mohsen-pashootan@users.noreply.github.com>
2020-07-22 18:38:57 +0430

    making input fields reusable and make short the attribute with spreading

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Form from "./common/form";
-import Input from "./common/input";
 import Joi from "joi-browser";
 
 class LoginForm extends Form {
@@ -20,28 +19,13 @@ class LoginForm extends Form {
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login Form</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("login")}
         </form>
       </div>
     );
